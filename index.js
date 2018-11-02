9d8130a4c1ad5a39081c4623d460f7f418817bc5
Matt Trussell <smtrussell@gmail.com>
2018-11-02 22:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+  const topDrivers = drivers.filter(function(driver, revenue){
+    return driver.revenue > revenue;
+  });
+  return topDrivers;
+}
+
+
+function driverNamesWithRevenueOver(){
+  
+}
+
+
+function exactMatch(){
+  
+}
+
+
+function exactMatchToList(){
+  
+}
